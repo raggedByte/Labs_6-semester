f5d12925fa8e17019e7ec2d5526190bb6d4db388
raggedByte <61023063+raggedByte@users.noreply.github.com>
2020-02-14 15:59:30 +0300

    Added crypt and decrypt function

--- MZKS_labs/lab2/lab2.cpp
+++ MZKS_labs/lab2/lab2.cpp
@@ -11,16 +11,84 @@
 
 #include <cstdio>
 
-int cryptBlock(unsigned int block)
-{
+typedef unsigned int Block;
 
-	return 0;
+int key[] = { 12, 13, 30, 5, 27, 6, 11, 25, 3, 21, 22, 2, 23, 0, 8, 4, 18, 19, 10, 1, 14, 29, 9, 28, 20, 17, 26, 31, 7, 16, 15, 24 };
+const unsigned int SIZE_BLOCK = (sizeof(key) / sizeof(int));
+
+int getBit(Block block, int numBit)
+{
+	return ((block & (1 << numBit)) != 0);
+}
+
+int setBit(Block block, int numBit)
+{
+	return (block | (1 << numBit));
+}
+
+int unsetBit(Block block, int numBit)
+{
+	return (block & ~(1 << numBit));
+}
+
+void printBlock(Block block, bool debug = false)
+{
+	if (debug)
+	{
+		printf("NumBit\t");
+		for (int i = SIZE_BLOCK; i > 0; i--)
+		{
+			printf("%3d", i);
+		}
+		printf("\n");
+	}
+	printf("Block\t");
+
+	for (int i = SIZE_BLOCK - 1; i >= 0; i--)
+	{
+		printf("%3d", getBit(block, i));
+	}
+	printf("\n");
+}
+
+void printKey()
+{
+	printf("Key\t");
+	for (int i = 0; i < SIZE_BLOCK; i++)
+	{
+		printf("%3d", key[i]);
+	}
+	printf("\n");
+}
+
+int cryptBlock(Block block)
+{
+	Block tempBlock = 0;
+	for (int i = 0; i < SIZE_BLOCK; i++)
+	{
+		int tempBit = getBit(block, key[i]);
+		if (tempBit == 1)
+			tempBlock = setBit(tempBlock, i);
+	}
+	return tempBlock;
+}
+
+int decryptBlock(Block block)
+{
+	Block tempBlock = 0;
+	for (int i = 0; i < SIZE_BLOCK; i++)
+	{
+		int tempBit = getBit(block, i);
+		if (tempBit == 1)
+			setBit(tempBlock, key[i]);
+	}
+	return tempBlock;
 }
 
 int main()
 {
-	FILE* pFile;
-	fopen_s(&pFile, "file.bin", "rb");
-	char buffer[256];
-	fread(buffer, 1, 5, pFile);
+	Block block = 0b11111;
+	printBlock(block);
+	printBlock(block, true);
+	printKey();
 }
